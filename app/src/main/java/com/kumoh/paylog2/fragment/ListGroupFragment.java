260b13c00d965eac4996d68363c8ef851334ba80
choi_hj94 <choihj94@gmail.com>
2019-10-02 04:55:36 +0900

    group 추가 FAB 구현 다이얼로그 샘플 구현

--- app/src/main/java/com/kumoh/paylog2/fragment/ListGroupFragment.java
+++ app/src/main/java/com/kumoh/paylog2/fragment/ListGroupFragment.java
@@ -1,6 +1,7 @@
 package com.kumoh.paylog2.fragment;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,10 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kumoh.paylog2.R;
 import com.kumoh.paylog2.adapter.GroupListRecyclerAdapter;
 import com.kumoh.paylog2.db.LocalDatabase;
 import com.kumoh.paylog2.db.PurchaseGroup;
+import com.kumoh.paylog2.db.PurchaseGroupDao;
+import com.kumoh.paylog2.dialog.NewGroupAddDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +52,44 @@ public class ListGroupFragment extends Fragment {
             adapter.setData(list);
         });
         // Inflate the layout for this fragment
+        FloatingActionButton groupAddFab = (FloatingActionButton) rootView.findViewById(R.id.add_group_fab);
+        groupAddFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.add_group_fab:
+                        NewGroupAddDialog dialog = new NewGroupAddDialog(getContext());
+                        dialog.setListener(new NewGroupAddDialog.NewGroupAddDialogListener() {
+                            @Override
+                            public void onAddButtonClicked(String groupName, String contents) {
+                                new InsertGroupAsyncTask(db.purchaseGroupDao()).execute(new PurchaseGroup(groupName,contents));
+                            }
+
+                            @Override
+                            public void onCancelButtonClicked() {
+
+                            }
+                        });
+                        dialog.show();
+                        break;
+                }
+            }
+        });
         return rootView;
+    }
+
+    private static class InsertGroupAsyncTask extends AsyncTask<PurchaseGroup, Void, Void>{
+
+        private PurchaseGroupDao dao;
+        public InsertGroupAsyncTask(PurchaseGroupDao dao){
+            this.dao = dao;
+        }
+        @Override
+        protected Void doInBackground(PurchaseGroup... purchaseGroups) {
+            dao.insertPurchaseGroup(purchaseGroups[0]);
+
+            return null;
+        }
     }
 
 }
