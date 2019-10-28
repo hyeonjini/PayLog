4171e2b690a65c9d9060bd9eab6aba2df00e55fe
JaeyeonG1(Laptop) <chlwodud2@naver.com>
2019-10-28 22:24:12 +0900

    구글 로그인 구현

---
+++ app/src/main/java/com/kumoh/paylog2/activity/LoginActivity.java
@@ -0,0 +1,121 @@
+package com.kumoh.paylog2.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.kumoh.paylog2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // 구글로그인 result 상수
+    private static final int RC_SIGN_IN = 900;
+    // 구글api클라이언트
+    private GoogleSignInClient googleSignInClient;
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
+    // 구글  로그인 버튼
+    private SignInButton buttonGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Firebase 인증 객체 선언
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        buttonGoogle = findViewById(R.id.btn_googleSignIn);
+        setGooglePlusButtonText(buttonGoogle,getString(R.string.google_login_text));
+
+        // Google 로그인을 앱에 통합
+        // GoogleSignInOptionsb 개체를 구성할 때 requestIdToken을 호출
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        buttonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 구글로그인 버튼 응답
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // 구글 로그인 성공
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+
+            }
+        }
+    }
+
+    // 사용자가 정상적으로 로그인한 후에 GoogleSignInAccount 개체에서 ID 토큰을 가져와서
+    // Firebase 사용자 인증 정보로 교환하고 Firebase 사용자 인증 정보를 사용해 Firebase에 인증합니다.
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Toast.makeText(LoginActivity.this, R.string.success_login, Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+                            finish();
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(LoginActivity.this, R.string.failed_login, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    // 구글 로그인 버튼 텍스트 설정 함수
+    private void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                return;
+            }
+        }
+    }
+}
--- app/src/main/java/com/kumoh/paylog2/activity/SplashActivity.java
+++ app/src/main/java/com/kumoh/paylog2/activity/SplashActivity.java
@@ -6,14 +6,37 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        //account가 null이 아닐 경우, 이 사용자는 이미 구글 로그인 상태이다.
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        finish();
+        if (user != null) { //이미 구글 로그인이 된(이 앱에 로그인 된) 상태
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
+        else { //로그인이 되지 않았으니, 로그인을 받아야 한다.
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
     }
 }
